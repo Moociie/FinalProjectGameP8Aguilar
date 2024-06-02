42a60aa99a762dcf4a9117401955cd15b8f1f797
Kimberly Aguilar <757379@student.dusd.net>
2024-06-02 00:56:59 -0700

    added shooting mechanic
    player should now shoot projectile

---
+++ FinalProject3DP8/Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float life = 3;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Destroy(gameObject, life);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+}
